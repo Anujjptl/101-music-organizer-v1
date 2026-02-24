1a77f2a447d669b0a729e2e334c3bb9d566c5e45
Anuj Patel <patelanuj452@gmail.com>
2026-02-24 15:12:40 -0500

    part II version 2

--- MusicOrganizer.java
+++ MusicOrganizer.java
@@ -94,8 +94,10 @@ public class MusicOrganizer
      * 
      */
     public void listAllFiles(){
+        int position = 0;
         for (String filename : files){
-                System.out.println(filename); 
+                System.out.println(position + ": " + filename);
+                position++;
         }
         }
     }
